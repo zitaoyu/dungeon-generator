889a609dd25719e328f8e13ae38bd8f31de08896
zitaoyu <zitaoyu.work@gmail.com>
2024-03-23 01:29:19 -0700

    2

--- main/dungeon_manager.gd
+++ main/dungeon_manager.gd
@@ -3,21 +3,46 @@ class_name DungeonManager
 
 @export var room: PackedScene = preload("res://main/room.tscn")
 
+# 2D array contains RoomData
 var dungeon_matrix: Array
-
-#temp
-var room_w = 16 * 5
-var room_h = 16 * 5
-var tile_size = 16
+# 2D array contains room nodes
+var rooms: Array = []
 
 func _ready():
 	dungeon_matrix = DungeonGenerator.generate_dungeon()
+	# initialize room nodes
 	for y in range(dungeon_matrix.size()):
+		var rooms_row = []
 		for x in range(dungeon_matrix[0].size()):
 			if dungeon_matrix[y][x] != null:
 				var r = room.instantiate()
-				r.position = Vector2(x * room_w, y * room_h)
-				add_child(r)
+				r.position = Vector2(x * Room.ROOM_WIDTH, y * Room.ROOM_HEIGHT)
+				rooms_row.append(r)
+			else:
+				rooms_row.append(null)
+		rooms.append(rooms_row)
+
+	# connect room nodes
+	for y in range(dungeon_matrix.size()):
+		for x in range(dungeon_matrix[0].size()):
+			var room_data = dungeon_matrix[y][x]
+			if room_data != null:
+				if room_data.left_room:
+					rooms[y][x].left_room = rooms[y][x - 1]
+					rooms[y][x - 1].right_room = rooms[y][x]
+				if room_data.right_room:
+					rooms[y][x].right_room = rooms[y][x + 1]
+					rooms[y][x + 1].left_room = rooms[y][x]
+				if room_data.top_room:
+					rooms[y][x].top_room = rooms[y - 1][x]
+					rooms[y - 1][x].bottom_room = rooms[y][x]
+				if room_data.bottom_room:
+					rooms[y][x].bottom_room = rooms[y + 1][x]
+					rooms[y + 1][x].top_room = rooms[y][x]
+
+	for row in rooms:
+		for room in row:
+			add_child(room)
 
 func _process(delta):
 	pass
--- main/room.gd
+++ main/room.gd
@@ -1,25 +1,34 @@
 extends Node2D
 class_name Room
 
+#temp
+const ROOM_WIDTH = 16 * 5
+const ROOM_HEIGHT = 16 * 5
+
 @export var left_room: Room = null
 @export var right_room: Room = null
 @export var top_room: Room = null
 @export var bottom_room: Room = null
-
+@onready var tile_map = $TileMap
 
 # Called when the node enters the scene tree for the first time.
-func _ready(): 
-	print($TileMap.get_layer_name(1))
-	var tile_map = $TileMap
-	var tile_map_layer = 0 
-	var tile_map_cell_position = Vector2i(0,0)
-	var tile_data = tile_map.get_cell_tile_data(tile_map_layer, tile_map_cell_position)
-	if tile_data:
-		var tile_map_cell_source_id = tile_map.get_cell_source_id(tile_map_layer, tile_map_cell_position); 
-		var tile_map_cell_atlas_coords = tile_map.get_cell_atlas_coords(tile_map_layer, tile_map_cell_position) 
-		var tile_map_cell_alternative = tile_map.get_cell_alternative_tile(tile_map_layer, tile_map_cell_position) 
-		var new_tile_map_cell_position = tile_map_cell_position + Vector2i.RIGHT 
-		tile_map.set_cell(tile_map_layer, new_tile_map_cell_position, tile_map_cell_source_id, tile_map_cell_atlas_coords, tile_map_cell_alternative)
+func _ready():
+	add_doors()
+
+func add_doors():
+	var tile_map_layer = 0
+	var tile_map_cell_source_id = 0
+	var tile_map_cell_atlas_coords = Vector2(3, 2)
+	var tile_map_cell_alternative = 0
+	#var new_tile_map_cell_position = Vector2i(0, 2)
+	if left_room:
+		tile_map.set_cell(tile_map_layer, Vector2i(0, 2), tile_map_cell_source_id, tile_map_cell_atlas_coords, tile_map_cell_alternative)
+	if right_room:
+		tile_map.set_cell(tile_map_layer, Vector2i(4, 2), tile_map_cell_source_id, tile_map_cell_atlas_coords, tile_map_cell_alternative)
+	if top_room:
+		tile_map.set_cell(tile_map_layer, Vector2i(2, 0), tile_map_cell_source_id, tile_map_cell_atlas_coords, tile_map_cell_alternative)
+	if bottom_room:
+		tile_map.set_cell(tile_map_layer, Vector2i(2, 4), tile_map_cell_source_id, tile_map_cell_atlas_coords, tile_map_cell_alternative)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
